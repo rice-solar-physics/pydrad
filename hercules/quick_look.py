adaabd59c1649e412163f5d70aee237bf52f6033
Will Barnes <wtb2@rice.edu>
2016-03-18 15:53:19 -0500

    adding movie maker function

--- hercules/quick_look.py
+++ hercules/quick_look.py
@@ -9,6 +9,7 @@ import os,sys
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import seaborn.apionly as sns
 
 class QuickViewer(object):
@@ -22,6 +23,11 @@ class QuickViewer(object):
     
     Examples
     --------
+    
+    Notes
+    --------
+    * make_profile_movie() function heavily based on this blog post 
+     (https://jakevdp.github.io/blog/2012/08/18/matplotlib-animation-tutorial/) from Jake VanderPlas
     
     """
     
@@ -100,6 +106,136 @@ class QuickViewer(object):
         #save or show the figure
         if print_fig_filename is not None:
             plt.savefig(print_fig_filename+'.'+self.fformat,format=self.fformat,dpi=self.dpi)
+        else:
+            plt.show()
+            
+            
+    def make_profile_movie(self,line_cm='coolwarm',movie_filename=None,interval=20,fps=30,**kwargs):
+        """Make movie showing loop profile at each time t"""
+        
+        if not hasattr(self,'results'):
+            raise AttributeError('No results found. Make sure to run self.load_results() first.')
+        
+        #colors
+        colors = sns.color_palette(line_cm,self.num_phy_files)
+        
+        #setup figure
+        fig,axes = plt.subplots(2,2,figsize=self.figsize,sharex=True)
+        #find and set static axes limits
+        min_t,max_t=1.e+300,0.
+        min_n,max_n=1.e+300,0.
+        min_v,max_v=1.e+300,0.
+        min_p,max_p=1.e+300,0.
+        for r in self.results:
+            #temperature
+            if r['Te'].min() < min_t or r['Ti'] < min_t:
+                min_t=min(r['Te'].min(),r['Ti'].min())
+            if r['Te'].max() > max_t or r['Ti'] > max_t:
+                max_t=max(r['Te'].max(),r['Ti'].max())
+            #density
+            if r['ne'].min() < min_n or r['ni'] < min_n:
+                min_n=min(r['ne'].min(),r['ni'].min())
+            if r['ne'].max() > max_n or r['ni'] > max_n:
+                max_n=max(r['ne'].max(),r['ni'].max())
+            #velocity
+            if r['v'].min() < min_v:
+                min_v=r['v'].min()
+            if r['v'].max() > max_v:
+                max_v=r['v'].max() 
+            #pressure
+            if r['pe'].min() < min_p or r['pi'] < min_p:
+                min_p=min(r['pe'].min(),r['pi'].min())
+            if r['pe'].max() > max_p or r['pi'] > max_p:
+                max_p=max(r['pe'].max(),r['pi'].max())
+            
+        #y axes
+        axes[0,0].set_ylim([min_t,max_t])
+        axes[0,1].set_ylim([min_n,max_n])
+        axes[1,0].set_ylim([min_v,max_v])
+        axes[1,1].set_ylim([min_p,max_p])        
+        #x axes    
+        axes[0,0].set_xlim([self.results[0]['s'][0],self.results[0]['s'][-1]])
+        axes[0,1].set_xlim([self.results[0]['s'][0],self.results[0]['s'][-1]])
+        axes[1,0].set_xlim([self.results[0]['s'][0],self.results[0]['s'][-1]])
+        axes[1,1].set_xlim([self.results[0]['s'][0],self.results[0]['s'][-1]])
+        
+        #labels
+        axes[0,0].set_ylabel(r'$T$ $(\mathrm{MK})$',fontsize=self.fontsize)
+        axes[0,1].set_ylabel(r'$n$  $(\mathrm{cm}^{-3})$',fontsize=self.fontsize)
+        axes[1,0].set_ylabel(r'$v$ $(\mathrm{cm/s})$',fontsize=self.fontsize)
+        axes[1,1].set_ylabel(r'$p$ $(\mathrm{dyne}\,\mathrm{cm}^{-2})$',fontsize=self.fontsize)
+        axes[1,0].set_xlabel(r'$s$ $(\mathrm{cm})$',fontsize=self.fontsize)
+        axes[1,1].set_xlabel(r'$s$ $(\mathrm{cm})$',fontsize=self.fontsize)
+        axes[0,1].set_yscale('log')
+        axes[1,1].set_yscale('log')
+        
+        plt.tight_layout()
+        
+        #setup plotters for each axes
+        lineTe,=axes[0,0].plot([],[],linestyle='-',linewidth=2)
+        lineTi,=axes[0,0].plot([],[],linestyle='--',linewidth=2)
+        linene,=axes[0,1].plot([],[],linestyle='-',linewidth=2)
+        lineni,=axes[0,1].plot([],[],linestyle='--',linewidth=2)
+        linev,=axes[1,0].plot([],[],linestyle='-',linewidth=2)
+        linepe,=axes[1,1].plot([],[],linestyle='-',linewidth=2)
+        linepi,=axes[1,1].plot([],[],linestyle='--',linewidth=2)
+        
+        #set title with current time
+        time_title=fig.suptitle('',fontsize=self.fontsize)
+        
+        #initialization function
+        def init():
+            """initialize animation"""
+            time_title.set_text('')
+            #temperature
+            lineTe.set_data([])
+            lineTe.set_color([])
+            lineTi.set_data([])
+            lineTi.set_color([])
+            #density
+            linene.set_data([])
+            linene.set_color([])
+            lineni.set_data([])
+            lineni.set_color([])
+            #velocity
+            linev.set_data([],[])
+            linev.set_color([])
+            #pressure
+            linepe.set_data([])
+            linepe.set_color([])
+            linepi.set_data([])
+            linepi.set_color([])
+        
+        #animation function
+        def animate(i):
+            r=self.results[i]
+            time_title.set_text(r'$t=%.2f$'%(self.time[i]))
+            #temperature
+            lineTe.set_data(r['s'],r['Te']/1e+6)
+            lineTe.set_color(colors[i])
+            lineTi.set_data(r['s'],r['Ti']/1e+6)
+            lineTi.set_color(colors[i])
+            #density
+            linene.set_data(r['s'],r['ne'])
+            linene.set_color(colors[i])
+            lineni.set_data(r['s'],r['ni'])
+            lineni.set_color(colors[i])
+            #velocity
+            linev.set_data(r['s'],r['v'])
+            linev.set_color(colors[i])
+            #pressure
+            linepe.set_data(r['s'],r['pe'])
+            linepe.set_color(colors[i])
+            linepi.set_data(r['s'],r['pi'])
+            linepi.set_color(colors[i])
+            return lineTe,lineTi,linene,lineni,linev,linepe,linepi,time_title
+            
+        #make the animation
+        anim=animation.FuncAnimation(fig,animate,init_func=init,frames=self.num_phy_files,blit=True,interval=interval,)
+        
+        #save or show
+        if movie_filename is not None:
+            anim.save(movie_filename,fps=fps,extra_args=['-vcodec','libx264'])
         else:
             plt.show()
             
