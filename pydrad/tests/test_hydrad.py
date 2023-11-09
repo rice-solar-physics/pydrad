e2e7e1447c122d32d963f85bce37fa7bb3ea5bc2
Will Barnes <will.t.barnes@gmail.com>
2023-11-08 21:52:42 -0500

    add a few smoke tests

---
+++ pydrad/tests/test_hydrad.py
@@ -0,0 +1,36 @@
+"""
+Some basic smoke tests for HYDRAD runs for different configurations
+"""
+import astropy.units as u
+
+from pydrad.configure import Configure
+from pydrad.configure.util import run_shell_command
+from pydrad.parse import Strand
+
+
+def test_beam_heating_run(tmpdir_factory, configuration, hydrad_clean):
+    configuration.config['heating']['beam'] = [
+        {
+            'time_start': 10*u.s,
+            'flux': 1e9 * u.Unit('erg cm-2 s-1'),
+            'cut_off': 15 * u.keV,
+            'index': 7,
+        },
+    ]
+    configuration.config['general']['total_time'] = 5 * u.s
+    hydrad_tmp = tmpdir_factory.mktemp('hydrad_tmp')
+    configuration.setup_simulation(hydrad_tmp, hydrad_clean, overwrite=True)
+    run_shell_command(hydrad_tmp / 'HYDRAD.exe')
+    strand = Strand(hydrad_tmp)
+    assert len(strand) == 6
+
+
+def test_use_openmp(tmpdir_factory, configuration, hydrad_clean):
+    omp_configuration = Configure(configuration.config, compiler='$CXX')
+    omp_configuration.config['general']['use_openmp'] = True
+    omp_configuration.config['general']['grid_cells_per_thread'] = 10
+    hydrad_tmp = tmpdir_factory.mktemp('hydrad_tmp')
+    omp_configuration.setup_simulation(hydrad_tmp, hydrad_clean, overwrite=True)
+    run_shell_command(hydrad_tmp / 'HYDRAD.exe')
+    strand = Strand(hydrad_tmp)
+    assert len(strand) == 3
