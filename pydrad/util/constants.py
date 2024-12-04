16cbbdd5f6d6005aaff2c7207e9d1080b542e765
Will Barnes <will.t.barnes@gmail.com>
2024-12-04 16:16:46 -0500

    avergage ion mass comment

--- pydrad/util/constants.py
+++ pydrad/util/constants.py
@@ -24,6 +24,7 @@ gamma = Constant(
 # https://github.com/rice-solar-physics/HYDRAD/blob/master/Resources/source/constants.h
 # and is the average ion mass for a H-He plasma computed by weighting the H and He ion
 # masses with a particular set of abundances.
+# m_avg_ion = m_H + \frac{n_{He}}{n_{H}} m_{He}
 m_avg_ion = Constant(
     abbrev='m_avg_ion',
     name='average ion mass for a H-He plasma',
